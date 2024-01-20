ff16a8e95f6e57e37c48558e557786b5ce78b4a7
Somesh Chavhan <someshchavhan904@gmail.com>
2024-01-20 18:50:12 +0530

    changed to create-react-app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+
+function App() {
+  return (
+    <main>
+      <p className='text-9xl'>
+        hello
+      </p>
+    </main>
+  );
+}
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,13 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "/node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "/node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      fontFamily: {
-        satoshi: ['Satoshi', 'sans-serif'],
-        inter: ['Inter', 'sans-serif'],
-      }
-    },
+    extend: {},
   },
   plugins: [],
-}
-
+});
